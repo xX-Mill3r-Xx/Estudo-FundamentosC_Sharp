93745a55a833d3424279fca20881dec8ec1f0edf
Miller Vasco <millermartinsvasco1@gmail.com>
2022-04-02 23:17:44 -0300

    Add Modulo003 FrameWorks

---
+++ 003FrameWorks/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace _003FrameWorks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* FrameWorks
+             * Definição:
+             * 
+             * - Framework é uma estrutura, um alicerce.
+             * - Conjunto de Blibliotecas.
+             * - Usamos como base para construir nossas aplicações e não precisar fazer tudo do zero;
+             * - Conceito existente em diversas tecnologias;
+             * - Veremos o .NET, um FrameWork da Microsoft;
+             */
+
+            /*
+             Conceitos.
+            - A ideia é trazer recursos comuns ja prontos,
+              - Itens do dia a dia
+              - Escrita de arquivos em disco por exemplo e acesso a dados;
+            - Validados e testados que nos poupam muito tempo;
+             */
+
+            /*-------------------------------------------------------------------------------------------------------------*/
+
+            /*.NET FrameWork
+             
+            - Definiçoes:
+
+            - C# é a linguagem o framework se chama .NET
+            - Teve sua primeira versão em meados de 2001 chamado de .NET Framework
+            Recebeu as versoes 1.0,1.1,2.0,3.0,3.5,4X(Atualmente)
+            - Pode ser instalado Side-by-Side
+            - Compativel somente com Windows
+            - Considerado Legado;
+             */
+
+            /*-------------------------------------------------------------------------------------------------------------*/
+
+            /*.NET Core
+             
+            Definiçoes:
+
+            - Versão moderna do .NET Framework
+            - Lançado em meados de 2015, veio para suportar SOs como linux e Mac
+            - Suas primeiras versoes continham apenas o basico
+            - Core significa nucleo, ou seja, o essencial;
+            - Foi totalmente reescrito
+            - Otima Retrocompatibilidade;
+
+            - Versão 3.1 esta bem estavel
+            - Recomendação atual da Microsoft
+            - Muitas melhorias de perfomace
+             */
+
+            /*-------------------------------------------------------------------------------------------------------------*/
+
+            /*.Net Standard
+            - .Net Framework e .NET Core coexistem, podendo inclusive serem instalados juntos e no mesmo projeto;
+
+            - Não é um framework, apenas um contrato, uma intersecção entre os frameworks
+            Garantindo que tudo que escrevemos será compativel com ambas plataformas;
+             */
+        }
+    }
+}
