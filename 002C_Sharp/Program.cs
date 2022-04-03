2a1445e8756b3e288ed45a258e78b659458b8579
Miller Vasco <millermartinsvasco1@gmail.com>
2022-04-02 22:33:16 -0300

    Add Modulo 002 Sobre o C#

---
+++ 002C_Sharp/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace _002C_Sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Sobre o C#
+             Definiçoes:
+
+            - Linguagem de programação
+             - Tipada, compilada e gerenciada.
+            - Criada por Anders Hejlseberg
+             - Em meados de 2001.
+            - Principal linguagem da Microsoft;
+             */
+
+            /*Porque aprender C#?
+             
+            - Linguagem completa
+            - Maturidade
+            - Mercado
+            - Comunidade
+            - Aplicabilidade
+            - Microsoft por tras
+            - Orientada a Objetos;
+             */
+
+            /*-------------------------------------------------------------------------------------------------------------*/
+
+            /*C# como primeira linguagem
+             
+            Definiçoes:
+
+            -Não é a mais facil
+            - Focada em OOP (Base da programação)
+             - Temos tambem a programação funcional
+            - Facil no começo pode prejudicar no final
+            - OOP te da a base para aprender coisas novas
+            - Quanto mais tempo na base, mais facil aprender coisas novas.
+            - A quantidade de horas é a mesma
+             */
+
+            /*-------------------------------------------------------------------------------------------------------------*/
+
+            /*Codigo gerenciado:
+             Definiçoes:
+
+            - Siguinifica que sua execução depende de um gerenciador, tambem conhecido como Runtime;
+            - No caso do C# este Runtime se chama CLR ou CLR Runtime;
+            - Gerencia a memoria, segurança, entre outros recursos basicos;
+
+            CLR - é a sigla para Common Language Runtime ou gerenciador de linguagem comum.
+
+            Em C/C++ por exemplo, fazemos alocação de memoria manualmente.
+            - Permite interagirmos menos ou quase não interagir com recursos da maquina;
+            */
+
+            /*-------------------------------------------------------------------------------------------------------------*/
+
+            /*Compilação e Gerenciamento
+             
+            Definiçoes;
+            - A Microsoft possui outras linguagens como VB.Net, F# e até Cobol.Net
+            - Todas precisam ser mantidas;
+            - Quando o ecossistema foi criado, ele foi pensado para abranger todas estas linguagens;
+
+            - Compiladores ficam separados um para cada linguagem;
+            - A compilação inicial gera um codigo intermediario, Intermediate language (IL)
+
+            Nota:(IL: Sigla para intermediate language ou linguagem intermediaria).
+
+            Definiçoes do IL;
+            - Embora as compilaçoes sejam diferentes, ogerenciamento é igual.
+            - Alocação de memoria,
+            - Execução de instruções no processador
+
+            - O CLR recebe um codigo e compila ele para IL
+             - C#, VB.Net, F#, Cobol.Net
+            - Podemos ter arquivos C# e VB.Net no mesmo projeto
+             - Nunca no mesmo arquivo;
+
+            O resultado da compilação do IL será sempre o mesmo;
+
+            O processo de convençã de IL para Binario é chamado de JIT;
+
+            Nota.:(JIT: Sigla para Just In Time)
+             */
+
+            /*-------------------------------------------------------------------------------------------------------------*/
+        }
+    }
+}
